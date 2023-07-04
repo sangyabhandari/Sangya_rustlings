8aa4bde953229765d7e1a9fddead5f0ab60575d4
sangyabhandari Github <sandishabhandari55@gmail.com>
2023-07-05 02:22:10 +0530

    Vec Done

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -11,9 +11,11 @@
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
+        *element*=2;
+
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+        
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -24,7 +26,7 @@ fn vec_map(v: &Vec<i32>) -> Vec<i32> {
     v.iter().map(|element| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        ???
+        element*2
     }).collect()
 }
 
