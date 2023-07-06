6183953bc2b90f9c1d59b1fb5b96b7b0babde376
sangyabhandari Github <sandishabhandari55@gmail.com>
2023-07-06 22:58:40 +0530

    Modules Done

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -7,11 +7,12 @@
 
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
+    
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+     pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -11,8 +11,8 @@
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    pub use self::fruits::PEAR as fruit;
+   pub  use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
@@ -29,6 +29,6 @@ fn main() {
     println!(
         "favorite snacks: {} and {}",
         delicious_snacks::fruit,
-        delicious_snacks::veggie
+        delicious_snacks::veggie,
     );
 }
